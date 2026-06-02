0bc2b71f5555832568359828e67c2597fa65f28c
theokalogr <theokalogr@gmail.com>
2026-06-02 21:40:49 +0300

    Add Point, Point2D, and Point3D classes for 2D and 3D point representation

---
+++ src/gr/aueb/cf/ch14/Point.java
@@ -0,0 +1,38 @@
+package gr.aueb.cf.ch14;
+
+public class Point {
+    private double x;
+
+    public Point() {
+
+    }
+
+    public Point(double x) {
+        this.x = x;
+    }
+
+    public double getX() {
+        return x;
+    }
+
+    public void setX(double x) {
+        this.x = x;
+    }
+
+    public void movePlusOne() {
+        x += 1;
+    }
+
+    protected void movePlus10() {
+        x += 10;
+    }
+
+    private void reset() {
+        x = 0;
+    }
+
+    @Override
+    public String toString() {
+        return "(" + x + ")";
+    }
+}
