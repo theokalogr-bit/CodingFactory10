4454713fb155f31f4a90a735cde926d480757776
theokalogr <theokalogr@gmail.com>
2026-06-09 23:25:11 +0300

    Add Main class to demonstrate usage of FlexibleNode

---
+++ src/gr/aueb/cf/ch17/Main.java
@@ -0,0 +1,11 @@
+package gr.aueb.cf.ch17;
+
+public class Main {
+
+    public static void main(String[] args) {
+        FlexibleNode flexibleNode = new FlexibleNode("Coding");
+        int value = (int) flexibleNode.getValue();
+
+        System.out.println(value);
+    }
+}
