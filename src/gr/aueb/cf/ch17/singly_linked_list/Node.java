71064f6acc512c30a9f99cbe100738447b36840f
theokalogr <theokalogr@gmail.com>
2026-06-11 22:00:57 +0300

    Add Node and SinglyList classes for generic singly linked list implementation

---
+++ src/gr/aueb/cf/ch17/singly_linked_list/Node.java
@@ -0,0 +1,22 @@
+package gr.aueb.cf.ch17.singly_linked_list;
+
+public class Node<T> {
+    private T item;
+    private Node<T> next;
+
+    public T getItem() {
+        return item;
+    }
+
+    public void setItem(T item) {
+        this.item = item;
+    }
+
+    public Node<T> getNext() {
+        return next;
+    }
+
+    public void setNext(Node<T> next) {
+        this.next = next;
+    }
+}
---
+++ src/gr/aueb/cf/ch17/singly_linked_list/SinglyList.java
@@ -0,0 +1,78 @@
+package gr.aueb.cf.ch17.singly_linked_list;
+
+public class SinglyList<T> {
+    private Node<T> head = null;
+
+    public void insertFirst(T t) {
+        Node<T> tmp = new Node<>();
+        tmp.setItem(t);
+        tmp.setNext(head);
+
+        head = tmp;
+    }
+
+    public void insertLast(T t) {
+        if (isEmpty()) {
+            insertFirst(t);
+            return;
+        }
+
+        Node<T> tmp = new Node<>();
+        tmp.setItem(t);
+        tmp.setNext(null);
+
+        Node<T> n;
+        for (n = head; n.getNext() != null; n = n.getNext());
+        n.setNext(tmp);
+    }
+
+    public Node<T> removeFirst() {
+        if (isEmpty()) return null;
+        Node<T> tmp = head;
+        head = head.getNext();
+        return tmp;
+    }
+
+    public Node<T> removeLast() {
+        if (isEmpty() || head.getNext() == null) {
+            return removeFirst();
+        }
+
+        Node<T> n;
+        for (n = head; n.getNext().getNext() != null; n = n.getNext());
+
+        Node<T> nodeToReturn = n.getNext();
+        n.setNext(null);
+        return nodeToReturn;
+    }
+
+    public Node<T> get(T t) {
+        Node<T> nodeToReturn = null;
+
+        for (Node<T> n = head; n != null; n = n.getNext()) {
+            if (n.getItem().equals(t)) {
+                nodeToReturn = n;
+                break;
+            }
+        }
+        return nodeToReturn;
+    }
+
+    public void traverse() {
+        for (Node<T> n = head; n != null; n = n.getNext()) {
+            System.out.println(n.getItem());
+        }
+    }
+
+    public int size() {
+        int counter = 0;
+        for (Node<T> n = head; n != null; n = n.getNext()) {
+            counter++;
+        }
+        return counter;
+    }
+
+    public boolean isEmpty() {
+        return head == null;
+    }
+}
