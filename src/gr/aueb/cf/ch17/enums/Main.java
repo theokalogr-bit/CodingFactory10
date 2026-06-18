cddc23d954064838ca045092025e69b8cd5922dc
theokalogr <theokalogr@gmail.com>
2026-06-18 18:35:01 +0300

    Add UserRole enum and Main class for demonstrating enum usage

---
+++ src/gr/aueb/cf/ch17/enums/Main.java
@@ -0,0 +1,17 @@
+package gr.aueb.cf.ch17.enums;
+
+public class Main {
+
+    public static void main(String[] args) {
+        UserRole userRole = UserRole.ADMIN;
+
+        String userRoleStr = userRole.name();       // ADMIN
+        UserRole userRole2 = UserRole.valueOf("ADMIN");
+
+        for (UserRole role : UserRole.values()) {
+            System.out.println(role.ordinal() + " - " + role.name());
+        }
+
+
+    }
+}
---
+++ src/gr/aueb/cf/ch17/enums/UserRole.java
@@ -0,0 +1,7 @@
+package gr.aueb.cf.ch17.enums;
+
+public enum UserRole {
+    ADMIN,      // 0
+    TEACHER,    // 1
+    STUDENT     // 2
+}
