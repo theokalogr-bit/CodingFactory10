f0823b25c05404f67983ffa7e8a3e78550e3c49c
theokalogr <theokalogr@gmail.com>
2026-06-25 21:04:45 +0300

    Add DBHelper class for database connection management

---
+++ src/gr/aueb/cf/ch18/bank_mysql/bankapp/core/DBHelper.java
@@ -0,0 +1,31 @@
+package gr.aueb.cf.ch18.bank_mysql.bankapp.core;
+
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+
+public class DBHelper {
+    private static final HikariDataSource datasource;
+
+    static {
+        HikariConfig config = new HikariConfig();
+
+
+
+        // Config DB
+        config.setJdbcUrl("jdbc:mysql://localhost:3306/bankdbdev?sslMode=preferred&serverTimezone=UTC&allowPublicKeyRetrieval=true");
+        config.setUsername("bankuser10");
+        config.setPassword("Ante123456!");
+
+        datasource = new HikariDataSource(config);
+    }
+
+    private DBHelper(){}
+
+    public static Connection getConnection() throws SQLException {
+        return datasource.getConnection();
+    }
+
+}
